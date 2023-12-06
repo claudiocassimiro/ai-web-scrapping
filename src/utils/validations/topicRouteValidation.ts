6e8a9efbf61f0fd75400373f7d3f4259b3406dce
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-12-06 17:08:03 -0300

    feat: implements validation to topic router

---
+++ src/utils/validations/topicRouteValidation.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const topicSchema = z.object({
+  topic: z.string(),
+  tags: z.array(z.string()).optional(),
+  TagsToAvoid: z.array(z.string()).optional(),
+  typeOfSearch: z.string().optional(),
+  typeOfReport: z.string().optional(),
+});
+
+export type TopicType = z.infer<typeof topicSchema>;
+
+export const topicSchemaValidation = (topic: TopicType) => {
+  const { success } = topicSchema.safeParse(topic);
+
+  return success;
+};
