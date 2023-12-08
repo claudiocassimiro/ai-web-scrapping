9e046c8f9dffd65d6c164ab80b51888a446cc124
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-12-08 10:26:03 -0300

    refactor: change function name

---
+++ src/utils/initializeChatOpenAI.ts
@@ -0,0 +1,11 @@
+import { ChatOpenAI } from "langchain/chat_models/openai";
+
+export const initializeChatOpenAI = () => {
+  const llm = new ChatOpenAI({
+    modelName: "gpt-3.5-turbo",
+    temperature: 0,
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+
+  return llm;
+};
