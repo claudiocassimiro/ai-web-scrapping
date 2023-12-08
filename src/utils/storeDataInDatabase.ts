1d281ab418a57ce42c0ad895f730fe5da73027b4
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-12-08 09:28:54 -0300

    feat: implements function to store data on db

---
+++ src/utils/storeDataInDatabase.ts
@@ -0,0 +1,48 @@
+import prisma from "../lib/prisma";
+import { PrismaVectorStore } from "langchain/vectorstores/prisma";
+import { Prisma, Document as PrismaDocument } from "@prisma/client";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { Document } from "langchain/dist/document";
+
+export const storeDataInDatabase = async (
+  splitedDocs: Document<Record<string, any>>[],
+) => {
+  const vectorStore = PrismaVectorStore.withModel<PrismaDocument>(
+    prisma,
+  ).create(new OpenAIEmbeddings(), {
+    prisma: Prisma,
+    tableName: "Document",
+    vectorColumnName: "vector",
+    columns: {
+      id: PrismaVectorStore.IdColumn,
+      content: PrismaVectorStore.ContentColumn,
+    },
+  });
+
+  await vectorStore.addModels(
+    await prisma.$transaction(
+      splitedDocs.map((content) =>
+        prisma.document.create({
+          data: {
+            content: content.pageContent,
+            media: content.metadata.media,
+            date: content.metadata.data,
+            link: content.metadata.link,
+            relatedTags: {
+              create: content.metadata.relatedTags,
+            },
+            relatedQuestions: {
+              create: content.metadata.relatedQuestions,
+            },
+            inlineVideos: {
+              create: content.metadata.inlineVideos,
+            },
+            inlineImages: {
+              create: content.metadata.inlineImages,
+            },
+          },
+        }),
+      ),
+    ),
+  );
+};
