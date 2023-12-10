1808595d5ff9f6c36124019b685d3fb61690100d
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-12-10 14:45:36 -0300

    refactor: pass new routers

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,24 @@
 import { Router } from "express";
-import topicController from "../controllers/topicController/handlerTopic";
+import topicController from "../controllers/Topic/handlerTopicController";
 import resolver from "../middleware/errorAdapter";
+import { authenticateToken } from "../middleware/authenticateToken";
+import { getSingleUserController } from "../controllers/User/getUserController";
+import { createUserController } from "../controllers/User/createUserController";
+import { authPasswordController } from "../controllers/Auth/authPasswordController";
+import { updateUserPasswordController } from "../controllers/User/updateUserController";
+import { deleteUserController } from "../controllers/User/deleteUserController";
+
 const router = Router();
 
-router.post("/api/topic", resolver(topicController.topic));
+router.post("/api/topic", authenticateToken, resolver(topicController.topic));
+router.get("/api/:id", authenticateToken, resolver(getSingleUserController));
+router.post("/api/register", resolver(createUserController));
+router.post("/api/login", resolver(authPasswordController));
+router.put(
+  "/api/:id",
+  authenticateToken,
+  resolver(updateUserPasswordController),
+);
+router.delete("/api/:id", authenticateToken, resolver(deleteUserController));
 
 export default router;
