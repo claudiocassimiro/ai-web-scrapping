9653717d33e5ca98c880a9a8218294f5b3f825ac
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-12-11 12:03:49 -0300

    refactor: add more temperature to model

--- src/utils/aiTools/initializeChatOpenAI.ts
+++ src/utils/aiTools/initializeChatOpenAI.ts
@@ -3,7 +3,7 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 export const initializeChatOpenAI = () => {
   const llm = new ChatOpenAI({
     modelName: "gpt-3.5-turbo",
-    temperature: 0,
+    temperature: 0.5,
     openAIApiKey: process.env.OPENAI_API_KEY,
   });
 
