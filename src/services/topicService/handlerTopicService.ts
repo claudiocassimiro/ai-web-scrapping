132fa1554a7704a99dc400c057d7328bf5cff56e
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-12-12 08:26:27 -0300

    refactor: apply some modification to permit new search type

--- src/services/topicService/handlerTopicService.ts
+++ src/services/topicService/handlerTopicService.ts
@@ -1,12 +1,12 @@
 import { TopicType } from "../../utils/validations/topicRouteValidation";
 import "dotenv/config";
-import { scrappingData } from "../../utils/documents/scrappingData";
-import { splitDocuments } from "../../utils/documents/splitDocuments";
 import { initializeChatOpenAI } from "../../utils/aiTools/initializeChatOpenAI";
-import { storeDataRepo } from "../../repositories/Topic/storeDataRepo";
 import { searchContextRepo } from "../../repositories/Topic/searchContextRepo";
 import { searchTopicRepo } from "../../repositories/Topic/searchTopicRepo";
 import { GenericErrorHandler } from "../../utils/errors/GenericErrorHandler";
+import scheduleReport from "../../utils/documents/scheduleReport";
+import { reportGenerator } from "../../utils/documents/reportGenerator";
+import { scheduleTopicRepo } from "../../repositories/Topic/scheduleTopicRepo";
 
 const handlerTopicService = async ({
   topic,
@@ -15,19 +15,33 @@ const handlerTopicService = async ({
   typeOfSearch,
   typeOfReport,
   email,
+  periodOfSearch,
 }: { email: string } & TopicType) => {
   const llm = initializeChatOpenAI();
   try {
+    if (typeOfSearch === "Busca Especifica") {
+      const scheduledTopicId = await scheduleTopicRepo({
+        topic,
+        tags,
+        tagsToAvoid,
+        typeOfReport,
+        email,
+      });
+
+      return scheduleReport({ scheduledTopicId, periodOfSearch });
+    }
+
     const thisTopicWasSerchedBefore = await searchTopicRepo(topic);
 
     if (!thisTopicWasSerchedBefore) {
-      const scrapedDataInDocs = await scrappingData(topic, tags, tagsToAvoid);
-
-      const splitedDocs = await splitDocuments(scrapedDataInDocs);
-
-      await storeDataRepo(splitedDocs, topic, email);
-
-      const message = await searchContextRepo(llm, typeOfReport, topic);
+      const message = reportGenerator({
+        topic,
+        tags,
+        tagsToAvoid,
+        typeOfSearch,
+        typeOfReport,
+        email,
+      });
 
       return message;
     }
